e94076d00eb4b8b965116399be843d3128c6ee58
riverabp <Lilly3799>
2020-10-26 19:25:34 -0400

    initial comment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import TodoList from './components/TodoList'
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super()
+    this.state = {
+      input: 'mali'
+    }
+  }
+
+  handleNameChange = () => {
+    console.log('here')
+    this.setState({
+      input: 'jackie'
+    })
+  }
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h1>hello Mali {this.state.input}</h1>
+        <button onClick={this.handleNameChange}>change to mali</button>
+        <TodoList />
       </div>
     );
   }
